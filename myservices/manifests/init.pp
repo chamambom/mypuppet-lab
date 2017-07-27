3f6a6ff45432a3e65a278931bfca108311329b78
mchamambo <martin@teamzol.co.zw>
2017-07-27 05:41:38 +0200

    adding new modules to test if the vcs repo is going to pull them and update them inside the puppet server

--- myservices/manifests/init.pp
+++ myservices/manifests/init.pp
@@ -1,19 +1,22 @@
 class myservices {
 
     package { 'vsftpd':
-
     ensure => 'present',
 
   }
 
     service { 'vsftpd':
-
     ensure => 'running',
-
     enable => true,
-
     require => Package['vsftpd'],
-
   }
+
+    package { 'httpd':
+    ensure => 'present',
+          }
+    service {'httpd':
+    ensure => 'running',
+    require => Package["httpd"],
+           }
 
 }
