f2f4f73268ca57acce88e5f19a33a6f4fdcbb8db
mchamambo <martin@teamzol.co.zw>
2017-08-03 00:54:30 +0200

    Testing a custom apache module

--- apache/manifests/configvhosts.pp
+++ apache/manifests/configvhosts.pp
@@ -1,34 +1,34 @@
-class apache::vhosts {
+class apache::configvhosts {
         
   if $::osfamily == 'RedHat' {
     file { '/etc/httpd/conf.d/vhost.conf':
       ensure    => file,
       content   => template('apache/vhosts-rh.conf.erb'),
     }
-    file { "/var/www/$hostname":
+    file { "/var/www/$fqdn":
       ensure    => directory,
     }
-    file { "/var/www/$hostname/public_html":
+    file { "/var/www/$fqdn/public_html":
       ensure    => directory,
     }
-    file { "/var/www/$hostname/log":
-    ensure    => directory,
-    }
+    #file { "/var/www/$hostname/log":
+    #ensure    => directory,
+    #}
         
   } elsif $::osfamily == 'Debian' {
-    file { "/etc/apache2/sites-available/$hostname.conf":
+    file { "/etc/apache2/sites-available/$fqdn.conf":
       ensure  => file,
       content  => template('apache/vhosts-deb.conf.erb'),
     }
-    file { "/var/www/$hostname":
+    file { "/var/www/$fqdn":
       ensure    => directory,
     }
-    file { "/var/www/html/$hostname/public_html":
+    file { "/var/www/html/$fqdn/public_html":
       ensure    => directory,
     }
-    file { "/var/www/html/$hostname/logs":
-      ensure    => directory,
-    }
+    #file { "/var/www/html/$hostname/logs":
+    #  ensure    => directory,
+    #}
   } else {
     print "This is not a supported distro."
   }
