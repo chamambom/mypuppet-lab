4194449acfa38efe4be3f3ba4cd68e54c3372ca4
mchamambo <martin@teamzol.co.zw>
2017-08-02 23:42:07 +0200

    Testing a custom apache module

--- myservices/manifests/init.pp
+++ myservices/manifests/init.pp
@@ -1,22 +1,21 @@
 class myservices {
-
+  
     package { 'vsftpd':
     ensure => 'present',
-
-  }
-
+    }
+    
     service { 'vsftpd':
     ensure => 'running',
     enable => true,
     require => Package['vsftpd'],
-  }
-
+    }
+    
     package { 'httpd':
-    ensure => 'present',
-          }
+              ensure => 'present',
+    }
+    
     service {'httpd':
-    ensure => 'running',
-    require => Package["httpd"],
-           }
-
+              ensure => 'running',
+              require => Package["httpd"],
+    }
 }
